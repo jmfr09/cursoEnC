95c3dfb41367fb934bcfad566767439b050299bb
jesusF <jmfloresr15@gmail.com>
2022-05-07 00:13:12 -0600

    ejercicio de la clase con switch en platzi

---
+++ switch.c
@@ -0,0 +1,32 @@
+//ejercicio de la clase con switch
+
+#include <stdio.h>
+
+int main(){
+    int option;
+
+
+    switch (option)
+    {
+    case 1:
+        printf("elegiste la opcion 1");
+        break;
+
+    case 2:
+        printf("escogiste la opcion 2");
+        break;
+
+    case 3:
+        printf("elegiste la opcion 3 padrino esta es la buena");
+        break;
+
+    default:
+        printf("ninguna de las 3 opciones elegistes al clabozo");
+        break;
+    }
+
+
+
+
+    return 0;
+}
